8cf753390a1587f14e2aa6fdf0106407d84e4a39
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-19 10:40:39 +0100

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁-𝗠𝗗 🐧
    🐧 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁-𝗠𝗗 🐧

---
+++ plugins/a-bsfem.js
@@ -0,0 +1,32 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.BsFem);
+};
+handler.command = /^(BsFem|BsFem)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.BsFem = `
+ 
+🫧 ⋆ ☇  𝗕𝗨𝗦𝗖𝗢 𝗩𝗦 𝗙𝗘𝗠 🦭
+
+          ›› 𝗛𝗼𝗿𝗮 ¦ 𝗠𝗼𝗱↷ 🩷
+          
+🎀𝙈𝙤𝙙𝙖𝙡𝙞𝙙𝙖𝙙:
+☁❩ 20:00 🇨🇱🇦🇷 ⌇ 18:00 🇵🇪
+🎀𝙈𝙤𝙙𝙖𝙡𝙞𝙙𝙖𝙙:
+☁❩ 22:00 🇨🇱🇦🇷 ⌇ 20:00 🇵🇪 
+🎀𝙈𝙤𝙙𝙖𝙡𝙞𝙙𝙖𝙙:
+☁❩ 00:00 🇨🇱🇦🇷 ⌇ 22:00 🇵🇪
+
+⊹ ִֶָ  🌷Mis Reglas, No modifico.
+⊹ ִֶָ  🌷Sin 🏳‍🌈 , Pc A Coordinar.
+⊹ ִֶָ  🌷Hablar Con IG Si O Si.
+⊹ ִֶָ  🌷Con logo En Mano.
+
+🎀Hablar:🎀
+wa.me/
+wa.me/
+
+`;
---
+++ plugins/a-bsmasc.js
@@ -0,0 +1,31 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.BsMasc);
+};
+handler.command = /^(BsMasc|BsMasc)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.BsMasc = `
+
+🗣️⋆ ☇  𝘽𝙐𝙎𝘾𝙊 𝙑𝙎 𝙈𝘼𝙎𝘾  👺
+
+          ›› 𝗛𝗼𝗿𝗮 ¦ 𝗠𝗼𝗱↷ 👺
+          
+🔱𝙈𝙤𝙙𝙖𝙡𝙞𝙙𝙖𝙙:
+☁❩ 20:00 🇨🇱🇦🇷 ⌇ 18:00 🇵🇪
+🔱𝙈𝙤𝙙𝙖𝙡𝙞𝙙𝙖𝙙:
+☁❩ 22:00 🇨🇱🇦🇷 ⌇ 20:00 🇵🇪 
+🔱𝙈𝙤𝙙𝙖𝙡𝙞𝙙𝙖𝙙:
+☁❩ 00:00 🇨🇱🇦🇷 ⌇ 22:00 🇵🇪
+
+⊹ ִֶָ  🌩️Mis reglas, No modifico
+⊹ ִֶָ  🌩️Pc A Coordinar
+⊹ ִֶָ  🌩️Hablar con IG si o si.
+⊹ ִֶָ  🌩️Con logo en mano
+
+🔱𝗛𝗮𝗯𝗹𝗮𝗿:🔱
+wa.me/
+wa.me/
+`;
