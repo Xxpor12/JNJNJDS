d33fd690fbe072a2dddaed44fd0e98352e9ff9cd
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-22 18:49:20 +0100

    Update owner-broadcastchats.js

--- plugins/owner-broadcastchats.js
+++ plugins/owner-broadcastchats.js
@@ -1,50 +1,38 @@
-import fs from 'fs';
-const handler = async (m, {conn, text} ) => {
-  const delay = (time) => new Promise((res) => setTimeout(res, time));
-  const chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map((v) => v[0]);
-  if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙴 𝚃𝚁𝙰𝙼𝙸𝚃𝙴*';
-  const cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m;
-  const teks = text ? text : cc.text;
-  for (const i of chats) {
-    await delay(500);
-    conn.relayMessage(i,
-        {liveLocationMessage: {
-          degreesLatitude: 35.685506276233525,
-          degreesLongitude: 139.75270667105852,
-          accuracyInMeters: 0,
-          degreesClockwiseFromMagneticNorth: 2,
-          caption: '[🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗖𝗼𝗺𝘂𝗻𝗶𝗰𝗮𝗱𝗼 🐧]\n\n' + teks + '\n\n🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗖𝗼𝗺𝘂𝗻𝗶𝗰𝗮𝗱𝗼 🐧',
-          sequenceNumber: 2,
-          timeOffset: 3,
-          contextInfo: m,
-        }}, {}).catch((_) => _);
-  }
-  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 ${chats.length} 𝙲𝙷𝙰𝚃𝚂 𝙿𝚁𝙸𝚅𝙰𝙳𝙾𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*`);
-};
-handler.help = ['broadcastchats', 'bcchats'].map((v) => v + ' <teks>');
-handler.tags = ['owner'];
-handler.command = /^(broadcastchats?|bcc(hats?)?)$/i;
-handler.rowner = true;
-export default handler;
-
-
-/* import fs from 'fs'
-let handler = async (m, { conn, text } ) => {
-let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
-let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-let teks = text ? text : cc.text
-for (let id of chats) {
-conn.sendButton(id, `*╔══❰ 𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎 ❱══╗*\n*║*\n*╠❧* ${text}\n*║*\n*╚══════════════╝*`, '𝙀𝙎𝙏𝙀 𝙀𝙎 𝙐𝙉 𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🤖 𝙾𝚆𝙽𝙴𝚁 🤖', '.owner'],['💎 𝙳𝙾𝙽𝙰𝚁 💎', '.donasi']], false, {
-contextInfo: { externalAdReply: {
-title: 'ᴄᴏᴍᴜɴɪᴄᴀᴅᴏ ᴏғɪᴄɪᴀʟ ᴀ ᴄʜᴀᴛs ᴘʀɪᴠᴀᴅᴏs',
-body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ',
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`,
-thumbnail: fs.readFileSync('./Menu2.jpg') }}})}
-m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 ${chats.length} 𝙲𝙷𝙰𝚃𝚂 𝙿𝚁𝙸𝚅𝙰𝙳𝙾𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*`)
-}
-handler.help = ['broadcastchats', 'bcchats'].map(v => v + ' <teks>')
+import { randomBytes } from 'crypto'
+let handler = async (m, { conn, command, participants, usedPrefix, text }) => {
+if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))        
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" } 
+let users = participants.map(u => conn.decodeJid(u.id))
+let cc2 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+let teks2 = text ? text : cc2.text 
+let d = new Date(new Date + 3600000)
+let locale = lenguajeGB.lenguaje()
+let dia = d.toLocaleDateString(locale, { weekday: 'long' })
+let fecha = d.toLocaleDateString(lenguajeGB.lenguaje(), { day: 'numeric', month: 'numeric', year: 'numeric' })
+let mes = d.toLocaleDateString(lenguajeGB.lenguaje(), { month: 'long' })
+let año = d.toLocaleDateString(lenguajeGB.lenguaje(), { year: 'numeric' })
+let tiempo = d.toLocaleString('es-CO', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
+let groups = Object.keys(await conn.groupFetchAllParticipating())
+let usersTag = participants.map(u => conn.decodeJid(u.id))
+let readMS = String.fromCharCode(8206).repeat(850)
+await m.reply(lenguajeGB.smsChatGP1())   
+for (let i = 0; i < groups.length; i++) {
+const id = groups[i];
+const infoGP = lenguajeGB.smsChatGP2(readMS, dia, mes, año, fecha, tiempo)
+const delay = i * 4000 //4 seg
+setTimeout(async () => { 
+await conn.sendMessage(id, { text: infoGP + teks2, mentions: [m.sender], mentions: (await conn.groupMetadata(id)).participants.map(v => v.id) }, { quoted: fkontak }) 
+//await conn.reply(id, infoGP + teks2, { mentions: (await conn.groupMetadata(id)).participants.map(v => v.id) }, { quoted: fkontak });
+}, delay)}         
+let totalGP = groups.length
+await m.reply(lenguajeGB.smsChatGP3(totalGP))
+}     
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcastchats?|bcc(hats?)?)$/i
-handler.rowner = true
-export default handler*/
+handler.command = /^(broadcast|bc)(group|grup|gc)$/i
+handler.owner = true
+export default handler
 
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+const delay = time => new Promise(res => setTimeout(res, time))
