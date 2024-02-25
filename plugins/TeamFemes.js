673e33d27717343d58272090326c769ad542124d
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-25 08:09:03 +0100

    🐧`𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧
    🐧`𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧

---
+++ plugins/TeamFemes.js
@@ -0,0 +1,24 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.TeamFem);
+};
+handler.command = /^(TeamFem)$/i;
+export default handler;
+
+global.TeamFem = `
+🐧`𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧
+
+*Time Long Fem 💋*
+https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
+
+*Team 216 Fem 💋*
+https://chat.whatsapp.com/FZ7eXEzXNxECjYOAnZ5YqW
+
+*Team 4A Fem 💋*
+https://chat.whatsapp.com/FhhRhBs8cZTGzQDdvd9qt1
+
+*Team Killersitas Fem 💋*
+https://chat.whatsapp.com/KIAj1FBLcFUAautJ7KnnZf
+
+*Team Storm Fem 💋*
+https://chat.whatsapp.com/GgNAgo4Gapy0jcuwBxetrN
+`;
---
+++ plugins/TeamMascus.js
@@ -0,0 +1,24 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.TeamMasc);
+};
+handler.command = /^(TeamMasc)$/i;
+export default handler;
+
+global.TeamMasc = `
+🐧`𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧
+
+*🦍 Team Time Long*
+https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
+
+*🦍 Clan Gaming*
+https://chat.whatsapp.com/HUdRVlBTRcK8FhUFypwyxI
+
+*🦍 Team K¡*
+https://chat.whatsapp.com/FhhRhBs8cZTGzQDdvd9qt1
+
+*🦍 Team Dk Gaming*
+https://chat.whatsapp.com/BdaLvDrY93lClHTNzFWraW
+
+*🦍 Team Xyz*
+https://chat.whatsapp.com/HPsFmXRfoLQCfEnGFw4vam
+`;
