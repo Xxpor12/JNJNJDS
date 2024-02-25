2bc709a3a8e762b53a39368ba88a6ae73388d0bd
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-25 08:46:36 +0100

    🐧𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧

--- plugins/TeamMascus.js
+++ plugins/TeamMascus.js
@@ -5,7 +5,7 @@ handler.command = /^(TeamMasc)$/i;
 export default handler;
 
 global.TeamMasc = `
-🐧`𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧
+🐧𝗣𝗶𝗻𝗴𝘂𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🐧
 
 *🦍 Team Time Long*
 https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
