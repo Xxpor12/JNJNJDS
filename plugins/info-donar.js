14477fb53c3d6f32aec6ea85f696d935ec7f125e
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 07:38:29 +0100

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗗𝗼𝗻𝗮𝗿 🐧

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,28 +1,56 @@
-let media = './media/menus/Menu4paypal.jpg'
-let handler = async (m, { conn, command }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-  let user = db.data.users[m.sender]
-let str = `𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗗𝗼𝗻𝗮𝗿 𝗘𝘀𝗰𝗿𝗶𝗯𝗲 𝗔𝗹 𝗦𝗶𝗴𝘂𝗶𝗲𝗻𝘁𝗲 𝗡𝘂𝗺𝗲𝗿𝗼 : +51 936 994 155`
-await conn.sendMessage(m.chat, { 
-text: str, 
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: true, 
-mentionedJid:[m.sender],
-"externalAdReply": {
-"showAdAttribution": true,
-"renderLargerThumbnail": true,
-"thumbnail": imagen1, 
-"title": `𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁`, 
-"containsAutoReply": true,
-"mediaType": 1, 
-"mediaUrl": paypal, 
-"sourceUrl": paypal, 
+let img = 'https://telegra.ph/file/effb4659c8f5e63937857.jpg'
+let dia = Math.floor(Math.random() * 30)
+let tok = Math.floor(Math.random() * 10)
+let hadesb = Math.floor(Math.random() * 4000)
+let expp = Math.floor(Math.random() * 5000)
+
+  global.db.data.users[m.sender].limit += dia
+  global.db.data.users[m.sender].money += hadesb
+  global.db.data.users[m.sender].joincount += tok
+  global.db.data.users[m.sender].exp += expp
+  
+let texto = `🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗗𝗼𝗻𝗮𝗿 🐧\n𝗘𝘀𝗲 𝗘𝘀 𝗘𝗹 𝗬𝗮𝗽𝗲 𝗘𝗻 𝗤𝗿 𝗣𝗼𝗿 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗔𝗽𝗼𝘆𝗮𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗔𝗹𝗴𝘂𝗻 𝗦𝗲𝗿𝘃𝗲𝗿 , 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗕𝗼𝘁 𝗢 𝗧𝗯𝗺 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗘𝗹 𝗡𝘂𝗺𝗲𝗿𝗼 𝗗𝗲𝗹 𝗬𝗮𝗽𝗲 𝗘𝘀 : 𝟵𝟯𝟲 𝟵𝟵𝟰 𝟭𝟱𝟱 💸`
+
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
 }
+await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
+global.db.data.users[m.sender].lastcofre = new Date * 1
 }
-}, { quoted: fkontak })}
-handler.command = /^dona(te|si)|donar|apoyar|paypal|donating$/i
-handler.exp = 80
+handler.help = ['daily']
+handler.tags = ['xp']
+handler.command = ['donar', 'apoyarpingui', 'donarowner'] 
+handler.register = true
 export default handler
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+
+  return hours + " Horas " + minutes + " Minutos"
+}
+}
