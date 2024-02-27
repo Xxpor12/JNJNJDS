b0f1fbc4fcf0fe4cd628fcb06b46b6e4f243a39b
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 07:55:58 +0100

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗗𝗼𝗻𝗮𝗿 🐧

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -2,7 +2,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://telegra.ph/file/effb4659c8f5e63937857.jpg'
+let img = 'https://telegra.ph/file/3411fb36017868cb3687c.jpg'
 let dia = Math.floor(Math.random() * 30)
 let tok = Math.floor(Math.random() * 10)
 let hadesb = Math.floor(Math.random() * 4000)
@@ -30,11 +30,12 @@ const fkontak = {
 	"participant": "0@s.whatsapp.net"
 }
 await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
+//await conn.sendButton(m.chat, texto, wm, img, [['🔰 𝙼𝙴𝙽𝚄', '/menu'] ], fkontak, m)  
 global.db.data.users[m.sender].lastcofre = new Date * 1
 }
 handler.help = ['daily']
 handler.tags = ['xp']
-handler.command = ['donar', 'apoyarpingui', 'donarowner'] 
+handler.command = ['agendasemanal', 'agenda', 'agendasemanal'] 
 handler.register = true
 export default handler
 
@@ -52,5 +53,4 @@ function msToTime(duration) {
   seconds = (seconds < 10) ? "0" + seconds : seconds
 
   return hours + " Horas " + minutes + " Minutos"
-}
 }
