fbd86858a9f466c7bafdb3b2ab7b61749fa2ef53
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 08:30:49 +0100

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -2,7 +2,7 @@ let handler = m => m
 handler.before = async function (m, { text, args, usedPrefix, command, conn } ) {
 let user = global.db.data.users[m.sender]
 if (user.afk > -1) {
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}✴️ *A F K* ✴️
+await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}🐧 *A F K* 🐧
 *▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
 *@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1']()}${user.afkReason ? `\n${lenguajeGB['smsAfkM2']()}👉 ` + user.afkReason : ''}
 
@@ -19,9 +19,9 @@ let afkTime = user.afk
 if (!afkTime || afkTime < 0)
 continue
 let reason = user.afkReason || ''
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
+await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}🐧 *A F K* 🐧
 *▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-😾 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeGB['smsAfkM6']()}`}
+🐧 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeGB['smsAfkM6']()}`}
 
 ${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
 }
