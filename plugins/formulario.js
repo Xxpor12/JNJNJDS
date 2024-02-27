b6b10bd3d1768333b1dc20e70980fa9199b1663a
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 01:47:50 +0100

    Update formulario.js

--- plugins/formulario.js
+++ plugins/formulario.js
@@ -6,14 +6,16 @@ export default handler;
 global.Formulario = `
 ♡𝅼  ͡   *REQUISITOS TEAM :* ۟   ָ֢    ֹ  
 
-*⎯★⃞💭 ﾞ Nombre:*
-*⎯★⃞💭 ﾞ Ig:*
-*⎯★⃞💭 ﾞ Nick del juego:*
-*⎯★⃞💭 ﾞ Edad:*
-*⎯★⃞💭 ﾞ Cuentas con cambio de nombre?:*
-*⎯★⃞💭 ﾞ En que destacas más?:*
-*⎯★⃞💭 ﾞ Cuentas con sala?:*
-*⎯★⃞💭 ﾞ Experiencia en vv2,500 y mapa?:*
+*⎯★⃞🐧 ﾞ Nombre :*
+*⎯★⃞🐧 ﾞ  Insta :*
+*⎯★⃞🐧 ﾞ Nick del juego :*
+*⎯★⃞🐧 ﾞ Edad :*
+*⎯★⃞🐧 ﾞ Cuentas con cambio de nombre? :*
+*⎯★⃞🐧 ﾞ En que destacas más? :*
+*⎯★⃞🐧 ﾞ Cuentas con sala? :*
+*⎯★⃞🐧 ﾞ Donarias 1 Sala Semanal? :*
+*⎯★⃞🐧 ﾞ Experiencia en vv2,500 y mapa? :*
+*⎯★⃞🐧 ﾞ Eres De Pc O Movil? :*
 
 *ᜊ 1 Vez Hayas Terminado @ A Cualquier Administrador*
 `;
