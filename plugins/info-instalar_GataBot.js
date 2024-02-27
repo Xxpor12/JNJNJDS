aa618995fc035e0e32360d23d12541d85017360c
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 08:16:51 +0100

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗜𝗻𝘀𝘁𝗮𝗹𝗮𝗿 🐧

--- plugins/info-instalar_GataBot.js
+++ plugins/info-instalar_GataBot.js
@@ -1,24 +1,11 @@
-let handler  = async (m, { conn, usedPrefix, command }) => {
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
-}
+const handler = async (m, {conn}) => {
+  m.reply(global.instalarbot);
+};
+handler.command = /^(instalarbot|instalarbot|instalarbot)$/i;
+export default handler;
 
-let picture = './media/menus/Menu1.jpg'
-let gata = ``
-await conn.sendFile(m.chat, gataImg.getRandom(), 'lp.jpg', gata, fkontak, false, { contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: '🐧 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁-𝗠𝗗 🐧', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})}
-/*conn.sendButton(m.chat, gata, `Comunícate con Mí Creadora si necesitas ayuda con la Instalación.\n\nContact My Creator if you need help with the Installation.\n\n${ig}\n${wm}`, picture, [
-['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentasgb'],
-['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar']], fkontak, m)}*/
-handler.command = /^(instalarbot)/i
-export default handler
+global.instalarbot = `
+🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗜𝗻𝘀𝘁𝗮𝗹𝗮𝗿 🐧
+
+𝗛𝗼𝗹𝗮 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗜𝗻𝘀𝘁𝗮𝗹𝗮𝗿 𝗘𝗹 𝗕𝗼𝘁 𝟭𝗿𝗼 𝗟𝗼 𝗧𝗶𝗲𝗻𝗲𝘀 𝗤𝘂𝗲 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗬 𝗘𝗹 𝗡𝘂𝗺𝗲𝗿𝗼 𝗘𝘀 +𝟱𝟭 𝟵𝟯𝟲 𝟵𝟵𝟰 𝟭𝟱𝟱
+`;
