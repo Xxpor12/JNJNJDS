3cf656a2fc7e457ae2fe3f0ce0809d64e269f4f7
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 08:06:59 +0100

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗘𝘀𝘁𝗮𝗱𝗼 🐧

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,30 +1,10 @@
-/**
-POR FAVOR TENGAN LA AMABILIDAD Y BONDAD DE NO CAMBIAR MÍNIMAMENTE LOS CRÉDITOS DE GATABOT-MD, 
-SI VAS A AÑADIR TUS DATOS O CRÉDITOS, ESTA BIEN. PERO NO QUITEN LOS QUE YA ESTAN DE GATABOT-MD, GRACIAS 
-**/
+const handler = async (m, {conn}) => {
+  m.reply(global.estado);
+};
+handler.command = /^(estado)$/i;
+export default handler;
+global.estado = `
+🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗘𝘀𝘁𝗮𝗱𝗼 🐧
 
-/** PLEASE BE KIND AND KINDNESS NOT TO MINIMALLY CHANGE GATABOT-MD CREDITS, 
-IF YOU ARE GOING TO ADD YOUR DATA OR CREDITS, IT'S OK. BUT DO NOT REMOVE THOSE THAT ARE ALREADY FROM GATABOT-MD, THANK YOU **/
-let handler = async (m, { conn, command, usedPrefix }) => {
-let picture = './media/menus/Menu1.jpg'
-let name = await conn.getName(m.sender)
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 1000) }) * 1000}
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let uptime = clockString(_uptime)
-let estado = ``
-await conn.sendFile(m.chat, gataImg.getRandom(), 'lp.jpg', estado, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: '𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})
-}
-handler.help = ['estado']
-handler.tags = ['main']
-handler.command = /^(estado|status|estate|state|heygata|stado|stats|botstat(us)?)$/i
-export default handler
-
-function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+𝗘𝗹 𝗘𝘀𝘁𝗮𝗱𝗼 𝗗𝗲 𝗣𝗶𝗻𝗴𝘂𝗶 𝗦𝗲 𝗘𝗻𝗰𝘂𝗲𝗻𝘁𝗿𝗮 𝗔𝗰𝘁𝗶𝘃𝗼 𝗟𝗮𝘀 𝟮𝟰 𝗛𝗼𝗿𝗮𝘀 𝗗𝗲𝗹 𝗗𝗶𝗮
+`;
