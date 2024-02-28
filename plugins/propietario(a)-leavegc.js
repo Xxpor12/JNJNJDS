52a01ed28656f1e27ae9c085954470bb9dad5c77
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 21:58:41 -0500

    Update propietario(a)-leavegc.js

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `*${wm} GRACIAS POR COMPRAR EL BOT SI DESEA RENOVAR ESCRIBE AL* +51 936 994 155`) 
+await conn.reply(id, `*${wm} GRACIAS POR COMPRAR EL BOT SI DESEA RENOVAR ESCRIBE AL* +51 981 983 121`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
