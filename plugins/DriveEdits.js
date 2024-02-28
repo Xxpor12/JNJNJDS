02ad877105707182d3d27da9453301538571d22b
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-27 20:27:38 -0500

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗘𝗱𝗶𝘁𝗼𝗿 🐧

--- plugins/DriveEdits.js
+++ plugins/DriveEdits.js
@@ -4,60 +4,39 @@ const handler = async (m, {conn}) => {
 handler.command = /^(DriveEdits|drivesdzn|recursosdzn2|dzn2)$/i;
 export default handler;
 global.DriveEdits = `
-𝗟𝗜𝗡𝗞𝗦 𝗗𝗥𝗜𝗩𝗘 𝗘𝗗𝗜𝗧𝗦
+🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗘𝗱𝗶𝘁𝗼𝗿 🐧
 
+𝗔𝗰𝗮 𝗧𝗲 𝗧𝗿𝗮𝗶𝗴𝗼 𝗨𝗻𝗼𝘀 𝗗𝗿𝗶𝘃𝗲𝘀 𝗣𝗮𝗿𝗮 𝗤𝘂𝗲 𝗘𝗺𝗽𝗶𝗲𝗰𝗲𝘀 𝗔 𝗘𝗱𝗶𝘁𝗮𝗿 𝗢 𝗣𝗮𝗿𝗮 𝗧𝘂𝘀 𝗘𝗱𝗶𝘁𝘀 :𝟯
 
-BALAS :
-https://mega.nz/folder/Wfgi3AxC#J8JLtus-jYyFG3HnN_NUlA
+𝗗𝗲𝘀𝘁𝗮𝗰𝗮𝗱𝗮𝘀 : https://drive.google.com/drive/folders/1SBkTyNjr2ZvJoSc7oCZfe6AzwSSdFgUG?usp=drive_link
 
+𝗥𝗲𝗰𝘂𝗿𝘀𝗼𝘀 𝗘𝘅𝗰𝗹𝘂𝘀𝗶𝘃𝗼𝘀 : https://drive.google.com/drive/folders/108IZsCfCLX5zw3y0FZ8LyscARbVVcu5p?usp=drive_link
 
-BRUSH :
-https://mega.nz/folder/bXxinBLT#OJYdlhQBjncyN82TTOW5ZA
+𝗗𝗿𝗶𝘃𝗲 𝗥𝗲𝗴𝗹𝗮𝘀 : https://drive.google.com/drive/folders/1e26By2qNhhgrM6ju9JJ8SBrSrjHsejwJ?usp=drive_link
 
+𝗗𝗿𝗶𝘃𝗲 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼 : https://drive.google.com/drive/folders/1BPQg9l6SkW5RpsnGCQHhP6gS83F8Siuz?usp=drive_link
 
-CADENAS:
-https://mega.nz/folder/XWhWVaoZ#coavE6O2Myw7OxWxFoClUg
+𝗕𝗮𝗹𝗮𝘀 : https://mega.nz/folder/Wfgi3AxC#J8JLtus-jYyFG3HnN_NUlA
 
+𝗕𝗿𝘂𝘀𝗵 : https://mega.nz/folder/bXxinBLT#OJYdlhQBjncyN82TTOW5ZA
 
-CAMISETAS :
-https://mega.nz/folder/aWQWzDwQ#Hdli0Dkn0IqjynRuii7Sgw
+𝗖𝗮𝗺𝗶𝘀𝗲𝘁𝗮𝘀 : https://mega.nz/folder/aWQWzDwQ#Hdli0Dkn0IqjynRuii7Sgw
 
+𝗗𝗿𝗶𝘃𝗲 𝗘𝗱𝗶𝘁𝘀 : https://drive.google.com/drive/folders/148_l0Rxkfmm9AWMxwN-oMG4x2706whAv / https://drive.google.com/drive/folders/1RMZdVco83CQ9X81gDDovVO5SZNU2BZXb / https://drive.google.com/drive/mobile/folders/15hjIAocSCZdK4v-nSgRb2Pp78rCMBMXY/1-PPXNyh0M51SusMSVPNF1d-Q7EkZUQDW/11ri1_tdrZP86PH8dWZZI2yFVPpH9PSgV?sort=13&direction=a / https://drive.google.com/drive/u/0/mobile/folders/1kWvht4sSR0ybvlZiI0SAIitCIrNgK_wA 
 
-DRIVES EDITS :
-https://drive.google.com/drive/folders/148_l0Rxkfmm9AWMxwN-oMG4x2706whAv
+𝗖𝗮𝗱𝗲𝗻𝗮𝘀 : https://mega.nz/folder/XWhWVaoZ#coavE6O2Myw7OxWxFoClUg
 
-https://drive.google.com/drive/folders/1RMZdVco83CQ9X81gDDovVO5SZNU2BZXb
+𝗗𝗲𝗴𝗿𝗮𝗱𝗮𝗱𝗼𝘀 : https://mega.nz/folder/LaI0HBJa#7_EDAma5WSTqMjEFDPvWyw
 
-https://drive.google.com/drive/mobile/folders/15hjIAocSCZdK4v-nSgRb2Pp78rCMBMXY/1-PPXNyh0M51SusMSVPNF1d-Q7EkZUQDW/11ri1_tdrZP86PH8dWZZI2yFVPpH9PSgV?sort=13&direction=a
+𝗗𝗲𝘀𝘁𝗮𝗰𝗮𝗱𝗮𝘀 𝗫𝟮 : https://drive.google.com/drive/folders/1SBkTyNjr2ZvJoSc7oCZfe6AzwSSdFgUG
 
-https://drive.google.com/drive/u/0/mobile/folders/1kWvht4sSR0ybvlZiI0SAIitCIrNgK_wA
+𝗘𝗺𝗽𝗿𝗲𝘀𝗮𝘀 : https://mega.nz/folder/OeR3iKjZ#pgi4Ka5I5b6bMrgAGwVsNw
 
+𝗙𝗼𝗻𝗱𝗼𝘀 : https://mega.nz/folder/DOgQFbhC#zQPYN3J9776vxuclCkNAwg
 
-DEGRADADOS :
-https://mega.nz/folder/LaI0HBJa#7_EDAma5WSTqMjEFDPvWyw
+𝗗𝗿𝗶𝘃𝗲 𝗙𝗲𝗺𝗲𝗻𝗶𝗻𝗼 : https://drive.google.com/drive/mobile/folders/17qXegvUYmXU0nYnMUJKJTSzGcChjQXyH
 
+𝗟𝗮𝗺𝗽𝗮𝗿𝗮𝘀 : https://mega.nz/folder/OPB1ESxS#1HxyBc_6dbNvYZxxIkwxbQ
 
-DESTACADAS :
-https://drive.google.com/drive/folders/1SBkTyNjr2ZvJoSc7oCZfe6AzwSSdFgUG
-
-
-EMPRESAS :
-https://mega.nz/folder/OeR3iKjZ#pgi4Ka5I5b6bMrgAGwVsNw
-
-
-FONDOS :
-https://mega.nz/folder/DOgQFbhC#zQPYN3J9776vxuclCkNAwg
-
-
-DRIVE FEM :
-https://drive.google.com/drive/mobile/folders/17qXegvUYmXU0nYnMUJKJTSzGcChjQXyH
-
-
-LAMPARAS :
-https://mega.nz/folder/OPB1ESxS#1HxyBc_6dbNvYZxxIkwxbQ
-
-
-LINEAS :
-https://mega.nz/folder/7epCiSbb#Vpx3e3Q5Aws-F-eOuE2O6Q
-
+𝗟𝗶𝗻𝗲𝗮𝘀 : https://mega.nz/folder/7epCiSbb#Vpx3e3Q5Aws-F-eOuE2O6Q
 `;
