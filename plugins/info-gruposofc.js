7ac17e1721a4e9bb9852dd56d9c611183b27c1e3
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-28 20:45:14 -0500

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗚𝗿𝘂𝗽𝗼𝘀 🐧

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -6,5 +6,6 @@ export default handler;
 global.gruposgb = `
 🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗚𝗿𝘂𝗽𝗼𝘀 🐧
 
-𝗛𝗼𝗹𝗮 𝗣𝗼𝗿 𝗘𝗹 𝗠𝗼𝗺𝗲𝗻𝘁𝗼 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁-𝗠𝗗 𝗡𝗼 𝗧𝗶𝗲𝗻𝗲 𝗡𝗶𝗻𝗴𝘂𝗻 𝗚𝗿𝘂𝗽𝗼 ;(
+𝗛𝗼𝗹𝗮 𝗘𝘀𝘁𝗲 𝗘𝘀 𝗘𝗹 𝗚𝗿𝘂𝗽𝗼 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗗𝗲 𝗣𝗶𝗻𝗴𝘂𝗶 𝗩𝗲𝗻𝘁𝗮𝘀 𝗬 𝗦𝗼𝗿𝘁𝗲𝗼𝘀 :𝟯
+https://chat.whatsapp.com/J108jej6L2CKdIo2qpBvh9
 `;
