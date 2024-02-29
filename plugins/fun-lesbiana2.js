5a79b97163328f50a13ce266adf579b0e957d4a1
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-02-29 07:59:56 -0500

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁-𝗠𝗗 🐧

--- plugins/fun-lesbiana2.js
+++ plugins/fun-lesbiana2.js
@@ -1,7 +1,7 @@
 var handler = async (m, {conn}) => {
 
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', { avatar: await conn.profilePictureUrl(who, "image").catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'), }), 'error.png', '*MIREN A ESTA LESBIANA JAJA 😂🏳️‍🌈*', m)
+conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lesbiana2', { avatar: await conn.profilePictureUrl(who, "image").catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'), }), 'error.png', '*MIREN A ESTA LESBIANA JAJA 😂🏳️‍🌈*', m)
 
 }
 handler.help = ['lesbiana2']
