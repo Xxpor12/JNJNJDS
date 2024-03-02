3787f6e349ba77541f8e7ddea329a8380144f85f
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-03-02 16:58:40 -0500

    🐧 𝗣𝗶𝗻𝗴𝘂𝗶𝗕𝗼𝘁-𝗠𝗗 🐧

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -1,4 +1,4 @@
-let generateWAMessageFromContent = (await import(global.baileys)).default
+import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
 import * as fs from 'fs';
 const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
   try {
@@ -18,7 +18,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     const isMedia = /image|video|sticker|audio/.test(mime);
     const more = String.fromCharCode(8206);
     const masss = more.repeat(850);
-    const htextos = `${text ? text : '*Hola :D*'}`;
+    const htextos = `${text ? text : '*Escribe nuevamente el texto Oe Gil*'}`;
     if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {image: mediax, mentions: users, caption: htextos, mentions: users}, {quoted: m});
@@ -32,11 +32,11 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: md}}}}}, {});
+      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: 'https://chat.whatsapp.com/IKeeFD8PDaS7rkyG7a12g1'}}}}}, {});
     }
   }
 };
-handler.command = /^(hidetag|noti|notify|viso|aviso)$/i
-handler.group = true
-handler.admin = true
-export default handler
+handler.command = /^(aviso|noti|notify|viso|hidetag)$/i;
+handler.group = true;
+handler.admin = true;
+export default handler;
