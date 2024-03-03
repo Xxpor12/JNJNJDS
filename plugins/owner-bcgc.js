5ec4778e8f134e2bc2cf3dafd011b42c2b09a774
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-03-03 05:22:23 -0500

    Update owner-bcgc.js

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -1,25 +1,4 @@
-import fs from 'fs'
-let handler = async (m, { conn, text } ) => {
-let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
-let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-let teks = text ? text : cc.text
-for (let id of groups) {
-conn.sendButton(id, `*╔══❰ 𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎 ❱══╗*\n*║*\n*╠❧* ${text}\n*║*\n*╚══════════════╝*`, '𝙀𝙎𝙏𝙀 𝙀𝙎 𝙐𝙉 𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🤖 𝙾𝚆𝙽𝙴𝚁 🤖', '.owner'],['💎 𝙳𝙾𝙽𝙰𝚁 💎', '.donasi']], false, {
-contextInfo: { externalAdReply: {
-title: 'ᴄᴏᴍᴜɴɪᴄᴀᴅᴏ ᴏғɪᴄɪᴀʟ ᴀ ɢʀᴜᴘᴏs',
-body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ',
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`,
-thumbnail: fs.readFileSync('./Menu2.jpg') }}})}
-m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 ${groups.length} 𝙶𝚁𝚄𝙿𝙾/𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*`)
-}
-handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
-handler.tags = ['owner']
-handler.command = /^(broadcast|bc)(group|grup|gc)$/i
-handler.rowner = true
-export default handler
-
-
-/* const handler = async (m, {conn, isROwner, text}) => {
+const handler = async (m, {conn, isROwner, text}) => {
   const delay = (time) => new Promise((res) => setTimeout(res, time));
   const getGroups = await conn.groupFetchAllParticipating();
   const groups = Object.entries(getGroups).slice(0).map((entry) => entry[1]);
@@ -35,7 +14,7 @@ export default handler
           degreesLongitude: 139.75270667105852,
           accuracyInMeters: 0,
           degreesClockwiseFromMagneticNorth: 2,
-          caption: '🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗖𝗼𝗺𝘂𝗻𝗶𝗰𝗮𝗱𝗼 🐧\n\n' + pesan + '\n\n🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗖𝗼𝗺𝘂𝗻𝗶𝗰𝗮𝗱𝗼 🐧',
+          caption: '' + pesan + '',
           sequenceNumber: 2,
           timeOffset: 3,
           contextInfo: m,
@@ -48,4 +27,4 @@ handler.tags = ['owner'];
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i;
 handler.owner = true;
 
-export default handler;*/
+export default handler;
