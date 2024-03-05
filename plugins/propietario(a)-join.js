2ad97c11a7afb8ccf78baea778d489239d6c39f5
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-03-05 09:10:50 -0500

    Add files via upload

--- plugins/propietario(a)-join.js
+++ plugins/propietario(a)-join.js
@@ -1,30 +1,27 @@
-lconst linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-let enviando;
-const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
- if (enviando) return;
-     enviando = true 
-  try {
-    const link = text //(m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
-    if (!link || !link.match(linkRegex)) throw '*[❗] Link erroneo o faltante, ingrese el enlace de un grupo de WhatsApp.*\n\n*—◉ Ejemplo:*\n*◉ #join https://chat.whatsapp.com/IKeeFD8PDaS7rkyG7a12g1*';
-    const [_, code] = link.match(linkRegex) || [];
-    if ( isPrems || isMods || isOwner || m.fromMe) {
-      const res = await conn.groupAcceptInvite(code);
-      await conn.sendMessage(m.chat, {text: '*[ ✔️ ] El Bot ha ingresado con éxito al grupo.*'}, {quoted: m})
-      enviando = false 
-    } else {
-      conn.sendMessage(m.chat, {text: '*[❗] El link de su grupo fue enviado a mi propietario/a.*\n\n*—◉ Su grupo estará en evaluación y el propietario/a del Bot decidirá si agrega o no al Bot.*\n\n*—◉ Algunas de las razones por la cual su solicitud puede ser rechazada son:*\n*1.- El Bot está saturado.*\n*2.- El Bot fue eliminado del grupo recientemente.*\n*3.- El link del grupo ha sido restablecido.*\n*4.-El Bot no se agrega a grupos por decisión del propietario/a.*\n\n*—◉ El proceso de evaluación puede tomar algo de tiempo, incluso dias, tenga paciencia.*'}, {quoted: m});
-      const data = global.owner.filter(([id]) => id)[0];
-      const dataArray = Array.isArray(data) ? data : [data];
-      for (const entry of dataArray) await conn.sendMessage(entry + '@s.whatsapp.net', {text: '*[❗] NUEVA SOLICITUD DE UN BOT PARA UN GRUPO [❗]*\n\n*—◉ Solicitante:* ' + '@' + m.sender.split('@')[0] + '\n*—◉ Link del grupo:* ' + link, mentions: [m.sender], contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [m.sender], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `${link}`, "sourceUrl": `${link}`}}}, {quoted: m});
-      enviando = false 
-    }
-  } catch {
-    enviando = false 
-    throw '*[❗] Lo sentimos, algo salio mal por favor reportelo o vuelva a intentarlo.*';
-  }
-};
-handler.help = ['join [chat.whatsapp.com]'];
-handler.tags = ['premium'];
-handler.command = /^join|nuevogrupo$/i;
-handler.private = true;
-export default handler;
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+
+let handler = async (m, { conn, text, isMods, isOwner }) => {
+let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
+let [_, code] = link.match(linkRegex) || []
+
+if (!code) throw `🐧 *Ingrese el enlace de un grupo.*`
+
+if ( isMods || isOwner || m.fromMe) {
+m.reply(`𝗣𝗶𝗻𝗴𝘂𝗶 𝗕𝗼𝘁 𝗦𝗲 𝗨𝗻𝗶𝗼 𝗖𝗼𝗿𝗿𝗲𝗰𝘁𝗮𝗺𝗲𝗻𝘁𝗲 𝗔𝗹 𝗚𝗿𝘂𝗽𝗼 🐧
+
+𝗥𝗲𝗰𝘂𝗲𝗿𝗱𝗮 𝗤𝘂𝗲 𝗦𝗶 𝗘𝗹 𝗕𝗼𝘁 𝗡𝗼 𝗦𝗲 𝗘𝗻𝗰𝘂𝗲𝗻𝘁𝗿𝗮 𝗘𝗻 𝗘𝗹 𝗚𝗿𝘂𝗽𝗼 𝗣𝗿𝗼𝗯𝗮𝗯𝗹𝗲𝗺𝗲𝗻𝘁𝗲 𝗟𝗼 𝗛𝗮𝘆𝗮𝗻 𝗦𝗮𝗰𝗮𝗱𝗼 𝗢 𝗘𝗹 𝗚𝗿𝘂𝗽𝗼 𝗘𝘀𝘁𝗲 𝗖𝗼𝗻𝗳𝗶𝗴𝘂𝗿𝗮𝗱𝗼 𝗣𝗮𝗿𝗮 𝗔𝗰𝗲𝗽𝘁𝗮𝗿 𝗠𝗶𝗲𝗺𝗯𝗿𝗼𝘀 🐧`)
+await delay(5 * 5000)
+let res = await conn.groupAcceptInvite(code)
+} else {
+const data = global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)
+
+await delay(5 * 5000)
+for (let jid of data.map(([id]) => [id] + '@s.whatsapp.net').filter(v => v != conn.user.jid)) m.reply(`${iig}╭══•───────────────•══╮\n┃ 📧 *𝙎𝙊𝙇𝙄𝘾𝙄𝙏𝙐𝘿 𝙋𝘼𝙍𝘼 𝙐𝙉 𝙂𝙍𝙐𝙋𝙊*\n┃ 📧 *𝙂𝙍𝙊𝙐𝙋 𝙍𝙀𝙌𝙐𝙀𝙎𝙏*\n╰══•───────────────•══╯\n\n*👤 𝙎𝙊𝙇𝙄𝘾𝙄𝙏𝘼𝙉𝙏𝙀 | 𝘼𝙋𝙋𝙇𝙄𝘾𝘼𝙉𝙏*\n` + ' wa.me/' + m.sender.split('@')[0] + '\n\n*🔮 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 | 𝙂𝙍𝙊𝙐𝙋 𝙇𝙄𝙉𝙆*\n ' + link, jid)
+
+m.reply(`${ag}*✅ TU ENLACE SE ENVIO A MI PROPIETARIO*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n⚠️ *SU GRUPO SERÁ EVALUADO Y QUEDARÁ A DE MI PROPIETARIO SI ${gt} SE UNE A TU GRUPO*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n *❕ES POSIBLE QUE SU SOLICITUD DE UNIR AL BOT A TU GRUPO SEA RECHAZADA POR LAS SIGUIENTES CAUSAS:*\n*1️⃣ El Bot está Saturado.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n2️⃣ *El Bot fue eliminado del Grupo.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n3️⃣ *El Grupo no cumple con las Normativas de ${gt}*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n4️⃣ *El enlace del grupo se restableció.*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n5️⃣ *No se agrega a Grupos según Mi Propietario(a).*\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n💌 *La solicitud puede tardar Horas en ser Respondida. Por favor Tener Paciencia. Gracias*`)}}
+
+handler.help = ['join [chat.whatsapp.com]']
+handler.tags = ['owner']
+handler.command = /^unete|join|nuevogrupo|unir|unite|unirse|entra|entrar$/i 
+export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
