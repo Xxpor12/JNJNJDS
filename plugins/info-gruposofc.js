7fbbe225a6a880f454e945eab77df5c4acafd7b0
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-03-11 12:48:39 +0100

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -6,6 +6,6 @@ export default handler;
 global.gruposgb = `
 🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗚𝗿𝘂𝗽𝗼𝘀 🐧
 
-𝗛𝗼𝗹𝗮 𝗘𝘀𝘁𝗲 𝗘𝘀 𝗘𝗹 𝗚𝗿𝘂𝗽𝗼 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗗𝗲 𝗣𝗶𝗻𝗴𝘂𝗶 :𝟯
-https://chat.whatsapp.com/IKeeFD8PDaS7rkyG7a12g1
+𝗛𝗼𝗹𝗮 𝗘𝘀𝘁𝗲 𝗘𝘀 𝗘𝗹 𝗚𝗿𝘂𝗽𝗼 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗗𝗲 𝗣𝗶𝗻𝗴𝘂𝗶 𝗖𝗼𝗺𝘂𝗻𝗶𝗱𝗮𝗱 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗗𝗲 𝗣𝗶𝗻𝗴𝘂𝗶 𝗕𝗼𝘁  :𝟯
+https://chat.whatsapp.com/Ghf2fHMzBLA1iuYAXSTOb4
 `;
