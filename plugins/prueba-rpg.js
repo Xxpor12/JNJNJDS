0062b45a4bae9c32869ac796240cc39723f04710
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-03-12 02:25:55 +0100

    Update prueba-rpg.js

--- plugins/prueba-rpg.js
+++ plugins/prueba-rpg.js
@@ -1,98 +1,13 @@
-import { areJidsSameUser } from "@whiskeysockets/baileys"
-const leaderboards = [
-"exp",
-"limit",
-"level",
-"role",
-"money",
-"joincount",
-"premium",  
-]
-
-let handler = async (m, { conn, args, participants, usedPrefix, command }) => {
-let users = Object.entries(global.db.data.users).map(([key, value]) => {
-return { ...value, jid: key }
-})
-  
-let leaderboard = leaderboards.filter(
-(v) => v && users.filter((user) => user && user[v]).length
-)
-
-let type = (args[0] || "").toLowerCase()
-const getPage = (item) =>
-Math.ceil(users.filter((user) => user && user[item]).length / 20);
-
-let wrong = `
-Uso: *${usedPrefix}${command} [elemento] [cantidad]*
-*${usedPrefix}${command} money 1*
-
-📍 Tipos de lista
-${leaderboard
-.map((v) =>
-`
-${rpg.emoticon(v)}${v}
-`.trim()
-)
-.join("\n")}
-`.trim();
-  
-if (!leaderboard.includes(type)) return m.reply(wrong);
-let page = isNumber(args[1]) ? Math.min(Math.max(parseInt(args[1]), 0), getPage(type)) : 0
-let sortedItem = users.map(toNumber(type)).sort(sort(type))
-let userItem = sortedItem.map(enumGetKey)
-// let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
-
-let text = `
-▣› *${rpg.emoticon(type)}${type} Leaderboard* ‹▣
-*📑 Página:* ${page} of ${getPage(type)}
-*🎖️ Tú:* *${userItem.indexOf(m.sender) + 1}* de *${userItem.length}*
-
-${sortedItem
-.slice(page * 20, page * 20 + 20)
-.map(
-(user, i) =>
-"▣\n" +
-`│ ${i + 1}〉 ${
-participants.some((p) => areJidsSameUser(user.jid, p.id))
-? `(${conn.getName(user.jid)}) wa.me/`
-: "@"
-}${user.jid.split`@`[0]}\n│▸ ${user[type]} ${type}${rpg.emoticon(type)}`
-).join`\n┗────────────·····\n\n`}
-┗────────────·····
-`.trim();
-return m.reply(text, null, {
-mentions: [...userItem.slice(page * 20, page * 20 + 20)].filter(
-(v) => !participants.some((p) => areJidsSameUser(v, p.id))
-),
-})
-}
-
-handler.command = /^(liga|league)$/i;
+const handler = async (m, {conn, command, usedPrefix, text}) => {
+  const which = command.replace(/ver/i, '');
+  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙰𝚁 *${usedPrefix}list${which}* 𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰*`;
+  const msgs = global.db.data.msgs;
+  if (!text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' 𝙽𝙾 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂*`;
+  const _m = await conn.serializeM(msgs[text]);
+  await _m.copyNForward(m.chat, true);
+};
+handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'get' + v + ' <text>');
+handler.tags = ['database'];
+handler.command = /^ver(vn|msg|video|audio|img|sticker)$/;
+handler.rowner = true;
 export default handler;
-
-function sort(property, ascending = true) {
-if (property)
-return (...args) =>
-args[ascending & 1][property] - args[!ascending & 1][property];
-else return (...args) => args[ascending & 1] - args[!ascending & 1];
-}
-
-function toNumber(property, _default = 0) {
-if (property)
-return (a, i, b) => {
-return {
-...b[i],
-[property]: a[property] === undefined ? _default : a[property],
-}}
-else return (a) => (a === undefined ? _default : a)
-}
-
-function enumGetKey(a) {
-return a.jid
-}
-
-function isNumber(number) {
-if (!number) return number
-number = parseInt(number)
-return typeof number == "number" && !isNaN(number)
-}
